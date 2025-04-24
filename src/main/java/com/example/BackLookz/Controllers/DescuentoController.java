f8c78c53dd13e87f33453116528c158103d8fe67
LucasJ27 <lucasjeremiasc@gmail.com>
2025-04-24 14:07:41 -0300

    Agrego gaguli

--- src/main/java/com/example/BackLookz/Controllers/DescuentoController.java
+++ src/main/java/com/example/BackLookz/Controllers/DescuentoController.java
@@ -1,13 +1,14 @@
 package com.example.BackLookz.Controllers;
 
 import com.example.BackLookz.Entities.Descuento;
+import com.example.BackLookz.Repositories.DescuentoRepository;
 import com.example.BackLookz.Services.DescuentoService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/descuentos") //"api" tenemos que modificarlo
-public class DescuentoController extends BaseController<Descuento, Long> {
+public class DescuentoController extends BaseController<Descuento, Long, DescuentoRepository, DescuentoService> {
 
     public DescuentoController(DescuentoService service) {
         super(service);
--- src/main/java/com/example/BackLookz/Controllers/PrecioController.java
+++ src/main/java/com/example/BackLookz/Controllers/PrecioController.java
@@ -1,13 +1,14 @@
 package com.example.BackLookz.Controllers;
 
 import com.example.BackLookz.Entities.Precio;
+import com.example.BackLookz.Repositories.PrecioRepository;
 import com.example.BackLookz.Services.PrecioService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/precios") //"api" tenemos que modificarlo
-public class PrecioController extends BaseController<Precio, Long> {
+public class PrecioController extends BaseController<Precio, Long, PrecioRepository,PrecioService> {
 
     public PrecioController(PrecioService service) {
         super(service);
