7e95ea5dd6ea61d7c07c85eb6e3220cf6f6eca1a
Santiago Comeglio <santicome05@gmail.com>
2025-06-09 12:29:54 -0300

    paginado productos

--- src/main/java/com/example/BackLookz/Repositories/ProductoRepository.java
+++ src/main/java/com/example/BackLookz/Repositories/ProductoRepository.java
@@ -1,6 +1,10 @@
 package com.example.BackLookz.Repositories;
 
 import com.example.BackLookz.Entities.Producto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface ProductoRepository extends BaseRepository<Producto, Long> {
+
+    Page<Producto> findByDisponibleTrue(Pageable pageable);
 }
--- src/main/java/com/example/BackLookz/Services/ProductoService.java
+++ src/main/java/com/example/BackLookz/Services/ProductoService.java
@@ -2,11 +2,22 @@ package com.example.BackLookz.Services;
 
 import com.example.BackLookz.Entities.Producto;
 import com.example.BackLookz.Repositories.ProductoRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductoService extends BaseService<Producto, Long, ProductoRepository> {
     public ProductoService(ProductoRepository repository) {
         super(repository);
+    }
+
+
+    public Page<Producto> obtenerPaginado(Pageable pageable) throws Exception {
+        try {
+            return repository.findByDisponibleTrue(pageable);
+        } catch (Exception e) {
+            throw new Exception("Error al obtener productos paginados: " + e.getMessage());
+        }
     }
 }
